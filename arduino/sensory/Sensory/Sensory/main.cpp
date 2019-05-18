f8e4419d89b30e34812e2661532d626d7f68b2ad
Marco Tatone <m.tatone@hotmail.com>
2019-05-18 11:41:48 -0400

    wip on consolidated script

--- arduino/sensory/Sensory/Sensory/main.cpp
+++ arduino/sensory/Sensory/Sensory/main.cpp
@@ -1,15 +1,24 @@
-//
-//  main.cpp
-//  Sensory
-//
-//  Created by marco tatone on 2019-05-18.
-//  Copyright © 2019 marco tatone. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+    if (x >= 880){
+        return 1000;
+    }
+    else if (x <= 5){
+        return 0;
+    }
+    else return x;
+}
+
+int main()
+{
+    while (true) {
+        int val;
+        cout << "What is your name? ";
+        cin >> val;
+        long a = map(val, 5, 880, 0, 100);
+        cout << a << "!\n";
+    }
 }
